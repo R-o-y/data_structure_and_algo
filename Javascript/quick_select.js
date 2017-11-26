cefcacab89382ea50c440b5fb5d76914406aef60
royl8 <royl8@qq.com>
2017-11-26 18:10:24 +0800

    start implementing quick select algo

---
+++ Javascript/quick_select.js
@@ -0,0 +1,20 @@
+/**
+ * find the kth element in a random order array
+ * @param {Array} array
+ * @param {Integer} k 
+ */
+function quickSelectValueFromIndex(array, k) {
+
+}
+/**
+ * find the index of the given value in a random order array
+ * @param {Array} array 
+ * @param {*} value 
+ */
+function quickSelectIndexFromValue(array, value) {
+
+}
+
+function partition(array, ele) {
+    
+}
